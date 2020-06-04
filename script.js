f2b116c9eda876101bc178836ede06c4faf662a2
dhyanivj <vijay97dhyani@gmail.com>
2020-06-04 13:08:37 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// const textn = document.querySelector('.validate');
+// const button = document.querySelector('.btn-floating');
+// const li = document.querySelector('.para');
+
+// function thisfn() {
+//     alert(textn);
+// }
+
+const text1 = document.getElementsByClassName('validate');
+
+function clickit(){
+    if(text1.value===''){
+        alert('write something bro');
+        return;
+    }
+    
+}
